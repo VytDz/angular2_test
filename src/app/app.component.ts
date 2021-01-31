f63c70462d4778336a80d6aa376e944c2b39901c
VytenisD <dzvonkasvytenis@gmail.com>
2021-01-31 22:53:04 +0200

    development: Try starter ag-grid tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+
   title = 'angular-test-by-v';
 
   columnDefs = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price'}
+    { field: 'make', sortable: true, filter: true, checkboxSelection: true },
+    { field: 'model', sortable: true, filter: true },
+    { field: 'price', sortable: true, filter: true },
   ];
 
-  rowData = [
-      { make: 'Toyota', model: 'Celica', price: 35000 },
-      { make: 'Ford', model: 'Mondeo', price: 32000 },
-      { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
+  rowData: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.rowData = this.http.get(
+      'https://www.ag-grid.com/example-assets/small-row-data.json'
+    );
+  }
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map((node) => node.make + ' ' + node.model)
+      .join(', ');
+
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
 }
